2ce3d8081165ed2579e987ac774a5addc0084f9b
maxh1t <m4xh17@gmail.com>
2026-02-11 15:44:45 -0300

    initial commit

---
+++ main.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const PORT = Number(process.env.PORT ?? 3001)
+
+const app = express();
+app.use(express.json());
+app.get("/health", (_, res) => res.json({ok: true}));
+
+app.get("/user/:id", (req, res) => {
+    const userId = Number(req.params.id);
+
+    res.json({
+        userId,
+        username: "demo"
+    });
+});
+
+app.listen(PORT, () => console.log(`auth listening on ${PORT}`));
